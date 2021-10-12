11d1537cbacd05744fd937033f8db9c958d9d338
Sotelo <cgonzalezsotelo@gmail.com>
2021-10-12 18:45:25 -0300

    autentificacion con vue router

---
+++ src/config/firebase-config.js
@@ -0,0 +1,13 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC0sUu13HSiZBoO9hTIPX5aNd0g6XcSbU0",
+
+  authDomain: "login-5fbac.firebaseapp.com",
+
+  projectId: "login-5fbac",
+
+  storageBucket: "login-5fbac.appspot.com",
+
+  messagingSenderId: "1096424867709",
+
+  appId: "1:1096424867709:web:a7b44c77a7ae96b317ee14",
+};
